09d83d4fe668e85f724eaac652247884cb215a77
Claire Conrardy (She/Her) <claire.conrardy@gmail.com>
2022-04-13 13:23:06 -0400

    Created the About card

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,9 +2,29 @@ import React from "react";
 
 export default function About() {
   return(
-    <div>
-      <p>Hello</p>
-      <p>My name is Claire</p>
+    <div id="about">
+      <div className="container">
+        <div className="about-card">
+          <p>
+            After a bachelor's degree in <strong style={{color: "#754F5B"}} >communication</strong> from EFAP Lille (France), I decided to move to Montreal in 2017 to start a new life.
+          </p>
+          <p>
+            During the last 4 years, I used to worked in the event industry. But with the pandemic, I wanted to change career. I wanted a career path where I could always <strong style={{color: "#754F5B"}}>learn and be challenged</strong>. That is why I turned into web development.
+          </p>
+          <p>
+            Now I freshly graduated from <strong style={{color: "#754F5B"}}>Le Wagon</strong> intensive coding bootcamp and discovered my real interest for <strong style={{color: "#754F5B"}}>front-end development</strong>. My past experiences in the event industry tought me how to <strong style={{color: "#754F5B"}}>handle stress</strong>, how to work in a <strong style={{color: "#754F5B"}}>fast-pace environment</strong> and how to <strong style={{color: "#754F5B"}}>deal with different types of clients</strong>.
+          </p>
+          <hr />
+          <p>
+            <strong>Fun facts:</strong>
+          </p>
+          <ul>
+            <li>I am a real <strong style={{color: "#754F5B"}}>bookworm</strong>: I try to read 40-50 books per year. <strong style={{color: "#754F5B"}}>Stephen King</strong> is my favorite author - I own almost 60 of his books. </li>
+            <li><strong style={{color: "#754F5B"}}>Rock music</strong> from the <strong style={{color: "#754F5B"}}>60s</strong> and <strong style={{color: "#754F5B"}}>70s</strong> is my favorite. I try to go to as much concerts as I can, when there is no pandemic.</li>
+            <li>I am a huge fan of <strong style={{color: "#754F5B"}}>Tarantino’s</strong>, <strong style={{color: "#754F5B"}}>Almodovar’s</strong> and <strong style={{color: "#754F5B"}}>Hitchock’s</strong> movies, and I almost know the French comedy La Cité de la Peur by heart.</li>
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
