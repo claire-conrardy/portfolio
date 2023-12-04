97d081e4bd20a15bf255f07afe99a84e6c4a6d10
Claire Conrardy (She/Her) <claire.conrardy@gmail.com>
2023-12-04 18:37:51 -0500

    change the profile section grid and add some fun facts about me

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -12,8 +12,20 @@ const Profile = () => {
         <div className="profile__text">
           <p>Salut, moi c'est Claire, et je suis développeuse <span>front-end</span>.</p>
           <p>Française d'origine, je suis devenue franco-canadienne en septembre 2023 après plus de 6 années passées à Montréal, où je suis toujours établie.</p>
-          <p>J'ai d'abord travaillé dans le domaine de l'événementiel, avant de me reconvertir dans le développement web suite à la pandémie.</p>
-          <p><span>Fun fact :</span> je suis une grande fan de metal mais aussi de... Francis Cabrel.</p>
+          <p>J'ai d'abord travaillé dans le domaine de l'événementiel, avant de me reconvertir dans le développement web suite à la pandémie. </p>
+          <p>J'aime pouvoir mettre à profit mon soucis du détail et l'apprentissage constant que </p>
+          <p><span>Fun facts :</span>
+          <ul>
+            <li>
+              Je suis une grande fan de musique rock (principalement années 60 et 70) et metal. Mais je suis aussi une grande fan de... Francis Cabrel.
+            </li>
+            <li>
+              Depuis 2009, j'ai assisté à plus de 200 concerts, dont 50 rien qu'en 2023 !
+            </li>
+            <li>
+              Je suis atteinte de <i>tsundoku</i>. J'achète constamment des livres que je n'ai pas le temps de lire (je lis quand même une trentaine de livres par an quand tout va bien).
+            </li>
+          </ul></p>
         </div>
       </div>
     </div>
